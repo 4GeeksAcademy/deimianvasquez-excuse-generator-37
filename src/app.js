e9aef844cdb2b5404cc7dbe57d2d0fab170acdca
Deimian Vásquez <deimianvasquez@gmail.com>
2024-10-08 00:33:02 +0000

    add excuse

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = document.getElementById("excuse");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+
+  console.log(randomWho, who.length);
+
+  excuse.innerHTML = `${who[randomWho]} ${action[randomAction]}`;
+
+  console.log(excuse);
 };
